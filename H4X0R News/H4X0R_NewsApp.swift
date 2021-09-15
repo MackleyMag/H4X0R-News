f085c6c025d761ba4f9202c6aec25ac030a007fc
MackleyMag <mackleymagalhaess@hotmail.com>
2021-09-15 06:32:56 -0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Mackley Magalhães da Silva on 15/09/21.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
